cfe21e12e587f88479501c76b8127c59a1555cac
youtubescholar <101683259+youtubescholar@users.noreply.github.com>
2024-08-03 18:56:47 -0400

    Create scripts.js

---
+++ homepage/class/scripts.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('.accordion-button').forEach(button => {
+    button.addEventListener('click', () => {
+        const accordionContent = button.nextElementSibling;
+        button.setAttribute('aria-expanded', button.getAttribute('aria-expanded') === 'false' ? 'true' : 'false');
+        accordionContent.hidden = !accordionContent.hidden;
+    });
+});
