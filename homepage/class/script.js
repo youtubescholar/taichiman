f9df266fb0c2e8cbce49b35c93b50fde8dce88b7
youtubescholar <101683259+youtubescholar@users.noreply.github.com>
2024-08-17 17:02:12 -0400

    Update script.js

--- homepage/class/script.js
+++ homepage/class/script.js
@@ -1,18 +1,27 @@
-
 // Main category panels
 document.querySelectorAll('.accordion-button').forEach(button => {
     button.addEventListener('click', function() {
-        this.classList.toggle('active');
         var panelContent = this.nextElementSibling;
-        panelContent.style.display = panelContent.style.display === 'block' ? 'none' : 'block';
+        if (panelContent.style.maxHeight) {
+            panelContent.style.maxHeight = null;
+            panelContent.style.opacity = 0;
+        } else {
+            panelContent.style.maxHeight = panelContent.scrollHeight + "px";
+            panelContent.style.opacity = 1;
+        }
     });
 });
 
 // Nested question panels within each category
 document.querySelectorAll('.sub-accordion').forEach(button => {
     button.addEventListener('click', function() {
-        this.classList.toggle('active');
         var subPanelContent = this.nextElementSibling;
-        subPanelContent.style.display = subPanelContent.style.display === 'block' ? 'none' : 'block';
+        if (subPanelContent.style.maxHeight) {
+            subPanelContent.style.maxHeight = null;
+            subPanelContent.style.opacity = 0;
+        } else {
+            subPanelContent.style.maxHeight = subPanelContent.scrollHeight + "px";
+            subPanelContent.style.opacity = 1;
+        }
     });
 });
