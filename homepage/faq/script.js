3719c8c5e371da3e61e65a561c753cb393ec5e46
youtubescholar <101683259+youtubescholar@users.noreply.github.com>
2024-08-17 19:47:48 -0400

    Update script.js

--- homepage/faq/script.js
+++ homepage/faq/script.js
@@ -1,7 +1,6 @@
 document.querySelectorAll('.accordion-button').forEach(button => {
     button.addEventListener('click', function() {
         var panelContent = this.nextElementSibling;
-        console.log("Accordion Clicked - Current max-height:", panelContent.style.maxHeight); // Log current max-height
         if (panelContent.style.maxHeight) {
             panelContent.style.maxHeight = null;
             panelContent.style.opacity = 0;
@@ -9,14 +8,12 @@ document.querySelectorAll('.accordion-button').forEach(button => {
             panelContent.style.maxHeight = panelContent.scrollHeight + "px";
             panelContent.style.opacity = 1;
         }
-        console.log("Accordion Clicked - New max-height:", panelContent.style.maxHeight); // Log new max-height
     });
 });
 
 document.querySelectorAll('.sub-accordion').forEach(button => {
     button.addEventListener('click', function() {
         var subPanelContent = this.nextElementSibling;
-        console.log("Sub-Accordion Clicked - Current max-height:", subPanelContent.style.maxHeight); // Log current max-height
         if (subPanelContent.style.maxHeight) {
             subPanelContent.style.maxHeight = null;
             subPanelContent.style.opacity = 0;
@@ -28,6 +25,5 @@ document.querySelectorAll('.sub-accordion').forEach(button => {
             subPanelContent.style.maxHeight = totalHeight + "px";
             subPanelContent.style.opacity = 1;
         }
-        console.log("Sub-Accordion Clicked - New max-height:", subPanelContent.style.maxHeight); // Log new max-height
     });
 });
